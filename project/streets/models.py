c85ceaf703823b1933796e238170751d75249d49
Roca <pavlov26.2008@gmail.com>
2025-03-27 20:01:27 +0700

    Added base html and added new model for posts, do migrate

--- project/streets/models.py
+++ project/streets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+	title = models.CharField(max_length=255)
+	content = models.TextField()
+
+	is_published = models.BooleanField(default=True)
--- project/streets/views.py
+++ project/streets/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
 
+posts = [
+	{'title': 'Test1', 'img': '', 'description': 'test descriptiondsfsdfsf sdfsdfsdfdfsdf sd sf sdf sfsdf sdfsdfsdf sdf sdf sdf sdf sdf sfdsd fsd'},
+	{'title': 'Test1', 'img': '', 'description': 'test descriptiondsfsdfsf sdfsdfsdfdfsdf sd sf sdf sfsdf sdfsdfsdf sdf sdf sdf sdf sdf sfdsd fsd'},
+	{'title': 'Test1', 'img': '', 'description': 'test descriptiondsfsdfsf sdfsdfsdfdfsdf sd sf sdf sfsdf sdfsdfsdf sdf sdf sdf sdf sdf sfdsd fsd'}
+]
+
+
 def index(request):
-	return render(request, 'index.html')
+	return render(request, 'index.html', {'posts': posts})
