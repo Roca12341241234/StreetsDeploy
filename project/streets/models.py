44b5abbf7d75e81b378573ed344598b5d0fa2fce
Roca <pavlov26.2008@gmail.com>
2025-03-28 19:33:49 +0700

    added new filed for slug urls

--- project/streets/models.py
+++ project/streets/models.py
@@ -5,6 +5,8 @@ class Posts(models.Model):
 	title = models.CharField(verbose_name='Заголовок', max_length=255)
 	description = models.TextField(verbose_name='Краткое описание')
 	content = models.TextField(verbose_name='Контент')
+	preview = models.ImageField(verbose_name='Картинка для превью', upload_to='photos/')
+	slug = models.SlugField(verbose_name='Слаг', blank=True, null=True, default='', max_length=255, db_index=True)
 
 	is_published = models.BooleanField(verbose_name='Опубликовать сразу', default=True)
 
@@ -17,7 +19,7 @@ class Posts(models.Model):
 
 class ImagesPost(models.Model):
 	post = models.ForeignKey(Posts, on_delete=models.CASCADE, blank=True, null=True, related_name='images')
-	image = models.ImageField(verbose_name='Картинка', upload_to='photos/')
+	image = models.ImageField(verbose_name='Картинка', upload_to=f'photos/')
 
 	def __str__(self):
 		return self.post.title
--- project/streets/views.py
+++ project/streets/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 
+from .models import Posts
 
 posts = [
 	{'title': 'Test1', 'img': '', 'description': 'test descriptiondsfsdfsf sdfsdfsdfdfsdf sd sf sdf sfsdf sdfsdfsdf sdf sdf sdf sdf sdf sfdsd fsd'},
@@ -9,4 +10,10 @@ posts = [
 
 
 def index(request):
+	posts = Posts.objects.all()
 	return render(request, 'index.html', {'posts': posts})
+
+def post(request, post_id):
+	post = get_object_or_404(Posts, pk=post_id)
+	
+	return render(request, 'post.html', {'post': post})
