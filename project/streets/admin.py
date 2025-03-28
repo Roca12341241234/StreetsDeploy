77c06c0e43f5e0cdfeacf0777386c8102de7ad63
Roca <pavlov26.2008@gmail.com>
2025-03-28 12:15:01 +0700

    Added new model for images to posts

--- project/streets/admin.py
+++ project/streets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Posts, ImagesPost
+
 
 # Register your models here.
+admin.site.register(Posts)
+admin.site.register(ImagesPost)
--- project/streets/models.py
+++ project/streets/models.py
@@ -2,7 +2,26 @@ from django.db import models
 
 
 class Posts(models.Model):
-	title = models.CharField(max_length=255)
-	content = models.TextField()
+	title = models.CharField(verbose_name='Заголовок', max_length=255)
+	description = models.TextField(verbose_name='Краткое описание')
+	content = models.TextField(verbose_name='Контент')
 
-	is_published = models.BooleanField(default=True)
+	is_published = models.BooleanField(verbose_name='Опубликовать сразу', default=True)
+
+	def __str__(self):
+		return self.title
+	
+	class Meta:
+		verbose_name = 'Пост'
+		verbose_name_plural = 'Посты'
+
+class ImagesPost(models.Model):
+	post = models.ForeignKey(Posts, on_delete=models.CASCADE, blank=True, null=True, related_name='images')
+	image = models.ImageField(verbose_name='Картинка', upload_to='photos/')
+
+	def __str__(self):
+		return self.post.title
+
+	class Meta:
+		verbose_name = 'Картинка'
+		verbose_name_plural = 'Картинки'
