b4c86d6b570045683ce2e0ae532d17cfb4bdc938
Roca <pavlov26.2008@gmail.com>
2025-04-14 18:06:42 +0700

    removed old code

--- project/streets/views.py
+++ project/streets/views.py
@@ -4,45 +4,35 @@ from django.utils.text import slugify
 from .models import Posts, Categories, ImagesPost
 from .forms import AddPostForm
 
-posts = [
-	{'title': 'Test1', 'img': '', 'description': 'test descriptiondsfsdfsf sdfsdfsdfdfsdf sd sf sdf sfsdf sdfsdfsdf sdf sdf sdf sdf sdf sfdsd fsd'},
-	{'title': 'Test1', 'img': '', 'description': 'test descriptiondsfsdfsf sdfsdfsdfdfsdf sd sf sdf sfsdf sdfsdfsdf sdf sdf sdf sdf sdf sfdsd fsd'},
-	{'title': 'Test1', 'img': '', 'description': 'test descriptiondsfsdfsf sdfsdfsdfdfsdf sd sf sdf sfsdf sdfsdfsdf sdf sdf sdf sdf sdf sfdsd fsd'}
-]
-
 
 def index(request):
 	posts = Posts.objects.filter(is_published=True)
 	categories = Categories.objects.all()
 
-	return render(request, 'index.html', {'categories': categories, 'posts': posts})
+	return render(request, 'index.html', {'categories': categories, 'posts': posts, 'scroll': True})
 
 def post(request, post_slug):
 	_post = Posts.objects.get(slug=post_slug)
 	images = ImagesPost.objects.filter(post = _post)
 	
-	return render(request, 'post.html', {'post': _post, 'images': images})
+	return render(request, 'post.html', {'post': _post, 'images': images, 'scroll': True})
 
 def category(request, category_slug):
 	posts = get_object_or_404(Categories, slug=category_slug)
 	posts = posts.category_posts.all()
 
-	return render(request, 'category_posts.html', {'posts': posts})
+	return render(request, 'category_posts.html', {'posts': posts, 'scroll': True})
 
 def add_post(request):
 	if request.method == 'POST':
 		form = AddPostForm(request.POST, request.FILES)
 		if form.is_valid():
-			# slug = slugify(form.cleaned_data['title'], allow_unicode=True)
-			new_post = Posts(
-				title=form.cleaned_data['title'],
-				description=form.cleaned_data['description'],
-				content=form.cleaned_data['content'],
-				preview=form.cleaned_data['preview'],
-				category=form.cleaned_data['category']
-			)
-			new_post.save()
+			form.save()
+			redirect('index')
 	else:
 		form = AddPostForm()
 
-	return render(request, 'add_post.html', {'form': form})
+	return render(request, 'add_post.html', {'form': form, 'scroll': False})
+
+def upload_video(request):
+	video = request.FILES.upload_files[0]
