1e5df37f3242918e3604edc7238c6ed08364cc91
Roca <pavlov26.2008@gmail.com>
2025-04-14 18:06:26 +0700

    removed comments from forms.py(removed text editors)

--- project/streets/forms.py
+++ project/streets/forms.py
@@ -2,19 +2,7 @@ from django import forms
 from .models import Posts, Categories
 
 
-
 class AddPostForm(forms.ModelForm):
 	class Meta:
 		model = Posts
-		# content = forms.CharField(widget=CKEditorWidget())
 		fields = '__all__'
-		# fields = ['title', 'description', 'content', 'preview', 'category']
-		# widgets = {
-		# 	'title': forms.TextInput(attrs={'class': 'form_item form_input'}),
-		# 	'description': forms.Textarea(attrs={
-		# 		'class': 'form_item form_description'
-		# 	}),
-		# 	# 'content': forms.CharField(widget=CKEditorWidget(config_name='awesome_ckeditor')),
-		# 	'category': forms.Select(attrs={'class': 'form_item'}),
-			
-		# }
