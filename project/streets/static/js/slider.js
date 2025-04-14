0a3407b82dba34fc1e14536e45ec9b8bf0fcd8c0
Roca <pavlov26.2008@gmail.com>
2025-04-14 18:01:32 +0700

    added function to run slider slider for query selecor(block of blocks)

--- project/streets/static/js/slider.js
+++ project/streets/static/js/slider.js
@@ -1,5 +1,30 @@
-$('.posts').slick({
-	infinite: true,
-	slidesToShow: 3,
-	slidesToScroll: 3
-});
+
+const runSlider = (selector) => {
+	$(selector).slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		responsive: [
+		  {
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 1,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 600,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1
+			}
+		  }
+		]
+	});
+} 
