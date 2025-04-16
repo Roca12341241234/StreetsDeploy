fda27399c2fea1c70e5abeb0678eb435bc626ebb
Roca <pavlov26.2008@gmail.com>
2025-04-16 18:42:13 +0700

    removed add_post page

--- project/streets/urls.py
+++ project/streets/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('post/<slug:post_slug>', views.post, name='post'),
 	path('category/<slug:category_slug>', views.category, name='category'),
-	path('add_post/', views.add_post, name='add_post'),
-
+	
 ]
