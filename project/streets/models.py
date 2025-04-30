1f57083a4a41fbe239e7f05727c6850b3da9fc68
Roca <pavlov26.2008@gmail.com>
2025-04-30 20:21:46 +0700

    created fiuled for upload video to post

--- project/streets/models.py
+++ project/streets/models.py
@@ -32,10 +32,11 @@ class Posts(models.Model):
 
 	content = HTMLField(verbose_name='Контент')
 	preview = models.ImageField(verbose_name='Картинка для превью', upload_to='photos/')
+	video = models.FileField(verbose_name='Видео', upload_to='videos/', blank=True, null=True)
 	slug = AutoSlugField(verbose_name='Слаг',unique=True, max_length=255, db_index=True, populate_from='title', slugify_function=lambda a: pytils.translit.slugify(a))
 	is_published = models.BooleanField(verbose_name='Опубликовать', default=True)
 
-	category = models.ForeignKey(Categories, on_delete=models.CASCADE, blank=True, null=True, related_name='category_posts')
+	category = models.ForeignKey(verbose_name='Категория поста', to=Categories, on_delete=models.CASCADE, blank=True, null=True, related_name='category_posts')
 
 
 	def __str__(self):
