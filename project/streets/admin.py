6d30db842271e57c4292b903e8c74bca233d76b2
Roca <pavlov26.2008@gmail.com>
2025-04-30 20:21:05 +0700

    created custom style for admin

--- project/streets/admin.py
+++ project/streets/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from .models import Posts, ImagesPost, Categories
+from .forms import AddPostForm
 
 
 class PostsAdmin(admin.ModelAdmin):
-	pass
-	# fields = ['title', 'description', 'content', 'preview', 'category', 'is_published']
-	# prepopulated_fields = {'slug': ('title',)}
+	form = AddPostForm
+	class Media:
+		css = {
+            "all": ["admin/css/admin_custom.css"],
+        }
 
 class CategoriesAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug': ('name',)}
+
 
 admin.site.register(Posts, PostsAdmin)
 admin.site.register(ImagesPost)
