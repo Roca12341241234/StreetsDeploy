13448412eff080bdeff0a0e0575906f575872b97
Roca <pavlov26.2008@gmail.com>
2025-05-09 11:48:12 +0700

    test migrations...

--- project/streets/views.py
+++ project/streets/views.py
@@ -1,28 +1,33 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect, HttpResponseRedirect
 from django.utils.text import slugify
+from django.forms.models import model_to_dict
 
 from .models import Posts, Categories, ImagesPost
 from .forms import AddPostForm, PostFilterForm
+from config.models import DefaultConfig, Participants
+
+default_config = DefaultConfig.objects.get()
+default_context = model_to_dict(default_config)
 
 
 def index(request):
 	posts = Posts.objects.filter(is_published=True)
 	categories = Categories.objects.all()
 
-	return render(request, 'index.html', {'categories': categories, 'slider_posts': posts[:5], 'scroll': True})
+	return render(request, 'index.html', {**default_context, 'categories': categories, 'posts': posts[:5], 'scroll': True})
 
 def post(request, post_slug):
 	_post = Posts.objects.get(slug=post_slug)
 	_post.views+=1
 	_post.save()
 
-	return render(request, 'post.html', {'post': _post, 'scroll': False})
+	return render(request, 'post.html', {**default_context, 'post': _post, 'scroll': False})
 
 def category(request, category_slug):
 	posts = get_object_or_404(Categories, slug=category_slug)
 	posts = posts.category_posts.all()
 
-	return render(request, 'category_posts.html', {'posts': posts, 'scroll': True})
+	return render(request, 'category_posts.html', {**default_context, 'posts': posts, 'scroll': True})
 
 def add_post(request):
 	if request.method == 'POST':
@@ -33,7 +38,7 @@ def add_post(request):
 	else:
 		form = AddPostForm()
 
-	return render(request, 'add_post.html', {'form': form, 'scroll': False})
+	return render(request, 'add_post.html', {**default_context, 'form': form, 'scroll': False})
 
 def posts(request):
 	_posts = Posts.objects.get_queryset()
@@ -62,4 +67,9 @@ def posts(request):
 			_posts = _posts.order_by('views')
 
 	
-	return render(request, 'posts.html', {'filters_form': filters_form, 'posts': _posts, 'scroll': False})
+	return render(request, 'posts.html', {**default_context, 'filters_form': filters_form, 'posts': _posts, 'scroll': False})
+
+def about(request):
+	participants = Participants.objects.all()
+
+	return render(request, 'about.html', {**default_context, 'participants': participants})
