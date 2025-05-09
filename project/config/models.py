c6d33eb933cc2a0d74078a92a659705eb6410f33
Roca <pavlov26.2008@gmail.com>
2025-05-09 11:46:40 +0700

    added models for config site(new app)

---
+++ project/config/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class DefaultConfig(models.Model):
+	project_name = models.CharField(verbose_name='Название проекта', max_length=255)
+	project_text = models.TextField(verbose_name='Текст о проекте')
+	
+	def __str__(self):
+		return 'Конфигурация'
+
+	class Meta:
+		verbose_name = 'Конфигурация сайта'
+		verbose_name_plural = 'Конфигурации сайта'
+
+
+class Participants(models.Model):
+	first_name = models.CharField(verbose_name='Имя', max_length=255)
+	last_name = models.CharField(verbose_name='Фамилия', max_length=255)
+
+	
+	def __str__(self):
+		return f'{self.last_name} {self.first_name}'
+	
+	class Meta:
+		verbose_name = 'Участник проекта'
+		verbose_name_plural = 'Участники проекта'
